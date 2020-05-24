a78bf6af320ff973016a5946a5606ee4bafcbdb3
mithicodes <mithicodes@users.noreply.github.com>
2020-05-24 15:12:07 +0530

    Add web build

---
+++ web-build/precache-manifest.62c4552afadcfb671392a48d4952745b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7701f88abb4e8c1c649",
+    "url": "/static/js/app.5bac1511.chunk.js"
+  },
+  {
+    "revision": "3636aebd34579cc662c5",
+    "url": "/static/js/runtime~app.4b0f5a04.js"
+  },
+  {
+    "revision": "45bc543db022cf718a95",
+    "url": "/static/js/2.0cf354d7.chunk.js"
+  },
+  {
+    "revision": "864b5a2a12e395a0aba167209a78df95",
+    "url": "/static/media/rock.864b5a2a.png"
+  },
+  {
+    "revision": "48d238a67d2b41996cd45194a93b7b10",
+    "url": "/static/media/scissors.48d238a6.png"
+  },
+  {
+    "revision": "cffb3100c0baea623b0353f9e933ef62",
+    "url": "/static/media/paper.cffb3100.png"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "766181a09beb389a31fa962866db9396",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "dec5f706fb840e294fa9221eaea16c10",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "cfeb3b51e7c8ae2850fae1ad801e8290",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "9a0d478598ccc3808ea6c8110c2459a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7551821bd19663ee64731bfb9f7fa2ee",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "4197a9fd61e635cea7da7906870ef364",
+    "url": "/register-service-worker.js"
+  }
+];
